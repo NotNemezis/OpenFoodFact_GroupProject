c13402d5a7c3ce0a387d03211af42a10eed5f96a
Grégoire Labadie <156668643+NotNemezis@users.noreply.github.com>
2025-03-24 15:46:34 +0100

    Update Importdata.py

--- Importdata.py
+++ Importdata.py
@@ -1,11 +1,1 @@
-import pandas as pd
-import os
 
-path = "https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.gz"
-df = pd.read_csv(path, nrows=10000, sep='\t',encoding="utf-8",low_memory=True,na_filter=True)
-
-df.to_csv("openfoodfacts_sample.csv", index=False, sep='\t', encoding="utf-8")
-
-os.system("git add openfoodfacts_sample.csv")
-os.system("git commit -m 'Ajout du fichier openfoodfacts_sample.csv'")
-os.system("git push origin main")
