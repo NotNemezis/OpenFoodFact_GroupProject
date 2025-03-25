e4999f4f3e4ff0298c1066fc3db20e54bdb2a2e9
lgregoir <gregoire.labadie@ynov.com>
2025-03-25 10:24:41 +0100

    Nettoyage

--- Importdata.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-path = "https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.gz"
-df = pd.read_csv(path, nrows=10000, sep='\t',encoding="utf-8",low_memory=True,na_filter=True)
-
-df.to_csv("openfoodfacts_sample.csv", index=False, sep='\t', encoding="utf-8")
